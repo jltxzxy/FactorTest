6df18d50a6e610fc81464b38804b068d5f2b81bc
jltxzxy <42018126+jltxzxy@users.noreply.github.com>
2022-08-15 11:35:06 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import setuptools
 setup(
     name='FactorTest',
-    version='1.0.5',
+    version='1.0.6',
     description='zxy 框架',
     long_description='详见https://github.com/jltxzxy/FactorTest.git',
     author='zhangxaingyu',
